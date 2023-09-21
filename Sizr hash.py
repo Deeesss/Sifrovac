d9abebaf7628d5e72eb3da8e37f76bcadf102181
Deeesss <111076028+Deeesss@users.noreply.github.com>
2023-09-21 11:46:14 +0200

    Update Sizr hash.py
    zmena kodu

--- Sizr hash.py
+++ Sizr hash.py
@@ -1,15 +1,15 @@
-retezec = ""
-print("Původní zpráva:", retezec)
-zprava = ""
+sifrovac = ""
+print("povodna sprava:", sifrovac)
+sprava = ""
 posun = 1
 
-for znak in retezec:
+for znak in sifrovac:
     i = ord(znak)
     i = i + posun
     if (i > ord("z")):
         i = i - 26
     znak = chr(i)
-    zprava = zprava + znak
+    sifrovac = sifrovac + znak
 
 print("Zašifrovaná zpráva:", zprava)
 input()
